ad30ed88d2031e50a13318791ec39ec57310c50e
Imaad Uddin <imaaduddin@icloud.com>
2021-06-26 23:37:09 -0500

    components and jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Loading
-        </a>
-      </header>
+      <Info />
+      <AddItem />
     </div>
   );
+}
+
+function Info() {
+  const title = "This is my title.";
+  const showTitle = true;
+
+  return (
+    <div>
+      <h1>{showTitle ? title : ""}</h1>
+      <h1>Inventory System</h1>
+      <p>Manage your stuff.</p>
+    </div>
+  )
+}
+
+function AddItem() {
+  return (
+    <div>
+      <label for="text-form">Type something:</label>
+      <input type="text" id="text-form"/>
+    </div>
+  )
 }
 
 export default App;
